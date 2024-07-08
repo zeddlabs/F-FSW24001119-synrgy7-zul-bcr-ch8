e8f70712f3c45b06250b888f01f731d654dbf270
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2024-07-08 15:22:12 +0700

    fix(user): remove avatar validation

--- app/routes/user.route.ts
+++ app/routes/user.route.ts
@@ -1,16 +1,21 @@
 import { Router } from "express"
 import controllers from "../controllers"
 import upload from "../middlewares/multer.middleware"
-import { check, checkSchema } from "express-validator"
-import { 
-  createUserValidationSchema, 
-  updateUserValidationSchema 
+import { checkSchema } from "express-validator"
+import {
+  createUserValidationSchema,
+  updateUserValidationSchema,
 } from "../schemas/user.schema"
-import { isAdmin, isSuperAdmin, jwtValidation } from "../middlewares/auth.middleware"
+import {
+  isAdmin,
+  isSuperAdmin,
+  jwtValidation,
+} from "../middlewares/auth.middleware"
 
 const userRouter: Router = Router()
 
-userRouter.route('/users')
+userRouter
+  .route("/users")
   .get(
     jwtValidation as any,
     isAdmin as any,
@@ -19,19 +24,13 @@ userRouter.route('/users')
   .post(
     jwtValidation as any,
     isSuperAdmin as any,
-    upload.single('avatar'),
-    check('avatar').custom((value, { req }) => {
-      if (!req.file) {
-        throw new Error('Avatar is required')
-      }
-
-      return true
-    }),
+    upload.single("avatar"),
     checkSchema(createUserValidationSchema),
     controllers.api.v1.userController.store
   )
 
-userRouter.route('/users/:id')
+userRouter
+  .route("/users/:id")
   .get(
     jwtValidation as any,
     isAdmin as any,
@@ -40,7 +39,7 @@ userRouter.route('/users/:id')
   .put(
     jwtValidation as any,
     isSuperAdmin as any,
-    upload.single('avatar'),
+    upload.single("avatar"),
     checkSchema(updateUserValidationSchema),
     controllers.api.v1.userController.update
   )
@@ -50,10 +49,8 @@ userRouter.route('/users/:id')
     controllers.api.v1.userController.destroy
   )
 
-userRouter.route('/user/current')
-  .get(
-    jwtValidation as any,
-    controllers.api.v1.userController.current as any
-  )
+userRouter
+  .route("/user/current")
+  .get(jwtValidation as any, controllers.api.v1.userController.current as any)
 
 export default userRouter
